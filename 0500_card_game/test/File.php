e6635fc6cc921012e701a70e4ec1ce03b564ca14
Takaaaki_z <takaaki.kit@gmail.com>
2016-09-05 22:30:00 +0900

    テストヘルパーにファイルパスを追加

--- 0500_card_game/test/File.php
+++ 0500_card_game/test/File.php
@@ -7,7 +7,7 @@ class FileTest extends Test_Base
 {
 	public function test_全てのデータを返す()
 	{
-		(new File('data/input.txt'))->write(['5 2','3 4']);
-		$this->assertEquals(['5 2', '3 4'], (new File('data/input.txt'))->read());
+		(new File(self::FILE_PATH))->write(['5 2','3 4']);
+		$this->assertEquals(['5 2', '3 4'], (new File(self::FILE_PATH))->read());
 	}
 }
--- 0500_card_game/test/Helper/base.php
+++ 0500_card_game/test/Helper/base.php
@@ -2,19 +2,24 @@
 
 class Test_Base extends PHPUnit_Framework_TestCase
 {
+	const FILE_PATH = 'data/input.txt';
+
 	public function setUp()
 	{
 		parent::setUp();
-		if(file_exists('data/input.txt')){
-			unlink('data/input.txt');
-		}
+		$this->delete_file();
 	}
 
 	#public function tearDown()
 	#{
 	#	parent::tearDown();
-	#	if(file_exists('data/input.txt')){
-	#		unlink('data/input.txt');
-	#	}
+	#	$this->delete_file();
 	#}
+	#
+	private function delete_file()
+	{
+		if(file_exists('data/input.txt')){
+			unlink('data/input.txt');
+		}
+	}
 }
